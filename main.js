a97e6772f84b1d76de6e8bcec7ee6208e0b185cc
rtobiwan <61915985+rtobiwan@users.noreply.github.com>
2022-07-20 18:29:33 -0400

    added html, css, js basic code

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-// add js
+const input = document.querySelector('input');
+const listBoard = document.querySelector('ul.todo-list');
+const addBtn = document.querySelector('.add');
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const task = document.createElement('li');
+  task.setAttribute('class', 'list-item');
+  task.textContent = input.value;
+
+  const delBtn = document.createElement('button');
+  delBtn.setAttribute('class', 'delete');
+  delBtn.textContent = '√';
+  task.append(delBtn);
+
+  const editBtn = document.createElement('button');
+  editBtn.setAttribute('class', 'edit');
+  editBtn.textContent = 'Edit';
+  task.append(editBtn);
+
+  listBoard.prepend(task);
+  input.value = '';
+
+  editBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    addBtn.textContent = 'Edit task';
+    delBtn.remove();
+    editBtn.remove();
+    input.value = task.textContent;
+
+    addBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      task.textContent = input.value;
+      task.remove();
+      input.value = '';
+      addBtn.textContent = 'Add task';
+    });
+  });
+
+  delBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    task.remove();
+  });
+});
